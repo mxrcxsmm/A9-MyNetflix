7e014de1d1996f8ac390290c7415d760c21911b3
Marcolo24 <100005655.joan23@fje.edu>
2025-02-20 16:04:04 +0100

    rutas

---
+++ proc/proc_register.php
@@ -0,0 +1,31 @@
+<?php
+
+include 'conexion.php';
+
+try {
+    // Preparar la consulta SQL para insertar un nuevo usuario
+    $sql = "INSERT INTO Usuario (nombre, apellidos, contrasena, correo, id_rol) VALUES (:nombre, :apellidos, :contrasena, :correo, :id_rol)";
+    $stmt = $pdo->prepare($sql);
+
+    // Asignar valores a los parámetros
+    $nombre = "Juan";
+    $apellidos = "Pérez";
+    $contrasena = password_hash("mi_contraseña_segura", PASSWORD_BCRYPT);
+    $correo = "juan.perez@example.com";
+    $id_rol = 1;
+
+    // Ejecutar la consulta
+    $stmt->execute([
+        ':nombre' => $nombre,
+        ':apellidos' => $apellidos,
+        ':contrasena' => $contrasena,
+        ':correo' => $correo,
+        ':id_rol' => $id_rol
+    ]);
+
+    echo "Usuario insertado correctamente.";
+} catch (PDOException $e) {
+    echo "Error al insertar usuario: " . $e->getMessage();
+}
+
+?>
--- view/registro.php
+++ view/registro.php
@@ -1,6 +1,6 @@
 <?php 
 
-include 'conexion.php';
+include '../conexion.php';
 
 ?>
 
@@ -11,12 +11,12 @@ include 'conexion.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="../css/style.css">
   </head>
 <body class="fondoRegistroInicioSesion justify-content-center align-items-center" style="min-height: 100vh;">
 <div class="container headerRegistro">
   <a href="index.php">
-    <img src="img/logoM&M.png" alt="logo" class="logo2">
+    <img src="../img/logoM&M.png" alt="logo" class="logo2">
   </a>
 </div>
 <div class="container d-flex justify-content-center align-items-center">
